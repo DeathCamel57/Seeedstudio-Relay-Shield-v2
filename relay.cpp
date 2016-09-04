4cea9ef3969169b9148d759527b281a3dd11c621
DeathCamel57 <DeathCamel57@gmail.com>
2016-09-04 01:51:08 -0400

    Added a check to make sure that the relay number is a value from 1 and 4.

--- relay.cpp
+++ relay.cpp
@@ -6,37 +6,39 @@
 #include "relay.h"
 
 relay::relay(int relaynum) {
-  pinMode(relaynum + 6, OUTPUT);
+  if (1 <= relaynum <= 4) {
+    pinMode(relaynum + 6, OUTPUT);
+  }
 }
 
 void relay::num1(int state) {
   if (state == 1) {
-    digitalWrite(7, HIGH);
+    digitalWrite(7, HIGH); // Turns on EL Wire
   } else if (state == 0) {
-    digitalWrite(7, LOW);
+    digitalWrite(7, LOW); // Turns off EL Wire
   }
 }
 
 void relay::num2(int state) {
   if (state == 1) {
-    digitalWrite(8, HIGH);
+    digitalWrite(8, HIGH); // Turns on EL Wire
   } else if (state == 0) {
-    digitalWrite(8, LOW);
+    digitalWrite(8, LOW); // Turns off EL Wire
   }
 }
 
 void relay::num3(int state) {
   if (state == 1) {
-    digitalWrite(9, HIGH);
+    digitalWrite(9, HIGH); // Turns on EL Wire
   } else if (state == 0) {
-    digitalWrite(9, LOW);
+    digitalWrite(9, LOW); // Turns off EL Wire
   }
 }
 
 void relay::num4(int state) {
   if (state == 1) {
-    digitalWrite(10, HIGH);
+    digitalWrite(10, HIGH); // Turns on EL Wire
   } else if (state == 0) {
-    digitalWrite(10, LOW);
+    digitalWrite(10, LOW); // Turns off EL Wire
   }
 }
